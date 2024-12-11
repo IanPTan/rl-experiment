b0579820eb1ee7829436fb4170d76314c639694b
IanPatrickMTan <ianparicktan@gmail.com>
2024-12-11 10:28:16 -0500

    added game batches, noise decay, and optimizer steps

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # What is this?
 I got bored so I decided to play around with tic-tac-toe and reinforcement learning. The approach is very naive, I randomly thought about it and wanted to test it out. Basically, one model (the actor) predicts the right move based on the current state of the board while another (the critic) predicts the success of that move. The critic is trained on game results to predict which moves lead to wins while the actor is trained on the critic's predictions of its success.
 # Tasks
-- game batches
-- noise decay
--- src/train.py
+++ src/train.py
@@ -12,7 +12,8 @@ def rbot(_):
     return pt.randn(9)
 
 
-def bot_test(bot, actor, max_games=1000):
+def bot_test(bot, actor, games=1000):
+    games = int(games)
     game = Game()
     actor.eval()
 
@@ -20,7 +21,8 @@ def bot_test(bot, actor, max_games=1000):
     wins = 0
     losses = 0
     turn = 0
-    for i in tqdm(range(max_games), desc="Playing...", unit="games"):
+    pbar = tqdm(range(games), desc="Playing...", unit="games")
+    for i in pbar:
         _, _, winner = bot_play(game, bot, actor, turn)
         game.reset()
         turn = 1 - turn
@@ -30,19 +32,21 @@ def bot_test(bot, actor, max_games=1000):
             wins += 1
         else:
             losses += 1
+        pbar.set_postfix(L=losses, D=draws, W=wins)
 
     return losses, draws, wins
         
 
 
-def bot_train(bot, actor, critic, actor_optimizer, critic_optimizer, max_games=100):
+def bot_train(bot, actor, critic, actor_optimizer, critic_optimizer, epochs=100):
+    epochs = int(epochs)
     game = Game()
     actor.train()
     critic.train()
 
 
     turn = 0
-    for i in tqdm(range(max_games), desc="Playing...", unit="games"):
+    for i in tqdm(range(int(epochs)), desc="Playing...", unit="epochs"):
         actor_optimizer.zero_grad()
         critic_optimizer.zero_grad()
         inputs, moves, winner = bot_play(game, bot, actor, turn)
@@ -69,45 +73,65 @@ def bot_train(bot, actor, critic, actor_optimizer, critic_optimizer, max_games=1
         actor_optimizer.step()
 
 
-def self_train(actor, critic, actor_optimizer, critic_optimizer, max_games=100, noise=0.2):
+def self_train(actor, critic, actor_optimizer, critic_optimizer, epochs=100, batches=1, steps=1, noise=0.2, decay=0.8):
+    epochs = int(epochs)
+    batches = int(batches)
+    steps = int(steps)
     game = Game()
     actor.train()
     critic.train()
 
 
-    for i in tqdm(range(max_games), desc="Playing...", unit="games"):
-        actor_optimizer.zero_grad()
-        critic_optimizer.zero_grad()
-        inputs, outputs, moves, winner = self_play(game, actor, noise)
-        game.reset()
-
+    pbar = tqdm(range(epochs), desc="Playing...", unit="epochs")
+    for i in pbar:
+        inputs = pt.zeros(0, 18)
+        outputs = pt.zeros(0, 9)
+        moves = pt.zeros(0, 9)
         results = pt.zeros(len(inputs))
-        if winner == -1:
-            results[:] = 0.5
-        else:
-            results[1 - winner::2] = 1
-        
-        move_states = pt.cat((inputs, moves), dim=1)
-        _, result_predictions = critic(move_states)
-        critic_loss = mse(result_predictions, results)
-        critic_loss.mean().backward()
-        critic_optimizer.step()
 
-        actor_inputs = inputs.detach()
-        actor_outputs = moves.detach()
-        output_states = pt.cat((actor_inputs, actor_outputs), dim=1)
-        actor_loss, _ = critic(output_states)
-        actor_loss.mean().backward()
-        actor_optimizer.step()
+        for j in range(batches):
+            game_inputs, game_outputs, game_moves, winner = self_play(game, actor, noise, decay)
+            game.reset()
+
+            game_results = pt.zeros(len(inputs))
+            if winner == -1:
+                game_results[:] = 0.5
+            else:
+                game_results[1 - winner::2] = 1
+
+
+            inputs = pt.cat((inputs, game_inputs))
+            outputs = pt.cat((outputs, game_outputs))
+            moves = pt.cat((moves, game_moves))
+            results = pt.cat((results, game_results))
+        
+        for k in range(steps):
+            critic_optimizer.zero_grad()
+            move_states = pt.cat((inputs.detach(), moves.detach()), dim=1)
+            _, result_predictions = critic(move_states)
+            critic_loss = mse(result_predictions, results)
+            critic_loss.mean().backward()
+            critic_optimizer.step()
+            pbar.set_postfix(loss=f"{critic_loss.mean():.4f}")
+
+        for l in range(steps):
+            actor_optimizer.zero_grad()
+            actor_inputs = inputs.detach()
+            actor_outputs = moves.detach()
+            output_states = pt.cat((actor_inputs, actor_outputs), dim=1)
+            actor_loss, _ = critic(output_states)
+            actor_loss.mean().backward()
+            actor_optimizer.step()
 
 
 if __name__ == "__main__":
     actor = Actor()
     critic = Critic()
-    actor_optimizer = pt.optim.Adam(actor.parameters(), lr=1e-3, weight_decay=1e-4)
+    actor_optimizer = pt.optim.Adam(actor.parameters(), lr=1e-2, weight_decay=1e-3)
     critic_optimizer = pt.optim.Adam(critic.parameters(), lr=1e-3, weight_decay=1e-4)
-    #self_train(actor, critic, actor_optimizer, critic_optimizer, 10**5, 0.2)
-    print(bot_test(rbot, actor, 10**4))
-    bot_train(rbot, actor, critic, actor_optimizer, critic_optimizer, 10**5)
-    print(bot_test(rbot, actor, 10**4))
+    print(bot_test(rbot, actor, 1e4))
+    #bot_train(rbot, actor, critic, actor_optimizer, critic_optimizer, 1e5)
+    for i in range(10):
+        self_train(actor, critic, actor_optimizer, critic_optimizer, 1e2, 1e2, 1e1, 0.2, 0.8)
+        print(bot_test(rbot, actor, 1e4))
     #_ = self_play(Game(), actor, 0.1, 1)
